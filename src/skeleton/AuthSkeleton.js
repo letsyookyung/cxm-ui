5c2393f9a3b536875bdfc5fcd5f2999d683183af
J Jay <jeongju@carrotins.com>
2023-09-25 17:24:13 +0900

    skeleton 스피너 추가

--- src/skeleton/AuthSkeleton.js
+++ src/skeleton/AuthSkeleton.js
@@ -1,3 +1,30 @@
-const MainSkeleton = () => <div>CXM 인중 확인 중...</div>
+// Material Dashboard 2 PRO React components
+import MDBox from "components_carrot/MDBox";
+import MDTypography from "components_carrot/MDTypography";
 
-export default MainSkeleton;
+// Spinner
+import PuffLoader from "react-spinners/PuffLoader";
+
+const AuthSkeleton = () => (
+  <MDBox 
+  display="flex"
+  width="100%" 
+  height="100%"
+  zIndex={1}
+  >
+    <MDBox
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      verticalAlign="middle"
+      mx="auto"
+    >
+      <PuffLoader size={35} color="#36d7b7" loading speedMultiplier={0.7} />
+      <MDTypography ml={1} variant="body2" color="text" fontWeight="regular">
+        Authenticating...
+      </MDTypography>
+    </MDBox>
+  </MDBox>
+  );
+
+export default AuthSkeleton;
--- src/skeleton/RootSkeleton.js
+++ src/skeleton/RootSkeleton.js
@@ -1,3 +1,3 @@
-const MainSkeleton = () => <div>CXM 초기 구성 중...</div>
+const RootSkeleton = () => <div>CXM 초기 구성 중...</div>
 
-export default MainSkeleton;
+export default RootSkeleton;
